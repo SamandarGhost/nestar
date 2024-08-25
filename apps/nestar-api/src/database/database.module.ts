82f75edcf6cba8e9473bfc0d48a36b574c260e25
Sam <kimjeyeon03@icloud.com>
2024-08-25 15:11:17 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -15,7 +15,7 @@ import { Connection } from 'mongoose';
 })
 export class DatabaseModule {
     constructor(@InjectConnection() private readonly connection: Connection) {
-        if(connection.readyState === 1) {
+        if (connection.readyState === 1) {
             console.log(
                 `MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`
             );
