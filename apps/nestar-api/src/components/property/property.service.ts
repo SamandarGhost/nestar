303a8d4c9136602802b111467555b04f6152169f
Sam <kimjeyeon03@icloud.com>
2024-09-12 16:35:25 +0900

    feat: develop boardArticle related graphQL APIa

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -57,13 +57,6 @@ export class PropertyService {
         targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
         return targetProperty;
     }
-
-    public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-        const { _id, targetKey, modifier } = input;
-        return await this.propertyModel.findByIdAndUpdate(
-            _id, { $inc: { [targetKey]: modifier } }, { new: true }).exec();
-    }
-
     public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
         let { propertyStatus, soldAt, deletedAt } = input;
         const search: T = {
@@ -241,4 +234,11 @@ export class PropertyService {
         if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
         return result;
     }
+
+    public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
+        const { _id, targetKey, modifier } = input;
+        return await this.propertyModel.findByIdAndUpdate(
+            _id, { $inc: { [targetKey]: modifier } }, { new: true }).exec();
+    }
+
 }
