c43cdeea71b4da4c7ca2ff1e7c9c8ae29f4487e6
Sam <kimjeyeon03@icloud.com>
2024-09-12 21:44:13 +0900

    feat: meLiked

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -56,6 +56,9 @@ export class PropertyService {
             }
 
             //  meLiked
+
+            const likeInput = { memberId: memberId, likeRefId: propertyId, likeGroup: LikeGroup.MEMBER };
+            targetProperty.meLiked = await this.likeService.checkLikeExistence(likeInput);
         }
 
         targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
