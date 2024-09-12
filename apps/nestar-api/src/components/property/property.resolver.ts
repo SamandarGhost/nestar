7ea1ab163dfc375e01f22b196973f7176f556ccf
Sam <kimjeyeon03@icloud.com>
2024-09-12 21:25:24 +0900

    feat: develop likeTargetProperty graphql api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -11,6 +11,8 @@ import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class PropertyResolver {
@@ -72,6 +74,18 @@ export class PropertyResolver {
     ): Promise<Properties> {
         console.log('Query: getAgentProperties');
         return await this.propertyService.getAgentProperties(memberId, input);
+
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation(() => Property)
+    public async likeTargetProperty(
+        @Args('input') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Property> {
+        console.log('Mutation: likeTargetProperty');
+        const likeRefId = shapeIntoMongoObjectId(input);
+        return await this.propertyService.likeTargetProperty(memberId, likeRefId);
 
     }
 
